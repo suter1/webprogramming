e3c0541c552998686e3fdd6cc88c2431c9987ec9
Flühmann Tobias, INI-ON-NTO-COY-NNI <tobias.fluehmann@swisscom.com>
2016-11-15 18:46:13 +0100

    div not self closing tag

--- data/site/templates/default/header.php
+++ data/site/templates/default/header.php
@@ -3,6 +3,7 @@
 <head>
     <link rel="stylesheet" type="text/css" href="/assets/styles/isithombe_default.css"/>
     <link rel="stylesheet" type="text/css" href="/assets/styles/forms.css"/>
+    <link rel="shortcut icon" type="image/x-icon" href="/assets/logo/icon_isithombe_web.ico">
     <meta charset="UTF-8"/>
     <title>Insert title here</title>
 </head>
@@ -10,7 +11,7 @@
     <div class="flex-container">
     <header>
         <a href="index.php">
-            <div class="logo"/>
+            <div class="logo"></div>
             <div class=headline><span>isithombe</span></div>
         </a>
     </header>
