355226371a2184b3efcda76c077a4039b7486639
Flühmann Tobias, INI-ON-NTO-COY-NNI <tobias.fluehmann@swisscom.com>
2016-11-22 19:41:36 +0100

    added seed data

--- data/site/db/seeds.php
+++ data/site/db/seeds.php
@@ -23,12 +23,36 @@ $languages = ['de' => [
 		'register' => 'Register',
 	],
 ];
+
+$admin_users = [
+	[
+		'first_name' => 'tobias',
+		'last_name' => 'flühmann',
+		'username' => 'tfluehmann',
+		'sex' => 'male',
+		'password_hash' => '$2y$10$W6zRHQBYFKRBkAHdpdbFhOfJL8xXeXcI6kRuAaBG5Lh8N0gCIAuL.',
+		'email' => 't.fluehmann@whatever.ch',
+	],
+	[
+		'first_name' => 'raphael',
+		'last_name' => 'suter',
+		'username' => 'rsuter',
+		'sex' => 'male',
+		'password_hash' => '$2y$10$W6zRHQBYFKRBkAHdpdbFhOfJL8xXeXcI6kRuAaBG5Lh8N0gCIAuL.',
+		'email' => 'r.suter@whatever.ch',
+	]
+];
 if($reinsert) {
 	Localization::delete_all();
 	foreach ($languages as $language => $entries) {
 		foreach($entries as $qualifier => $value){
 			Localization::create(['qualifier' => $qualifier, 'value' => $value, 'lang' => $language]);
 		}
+	}
+
+	User::delete_all();
+	foreach ($admin_users as $user){
+		User::create($user);
 	}
 }
 $database->disconnect();
