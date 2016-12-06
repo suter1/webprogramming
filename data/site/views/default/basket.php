a68377e38dbb63d1ab23b6d77d0dd62dd1f25de9
Flühmann Tobias, INI-ON-NTO-COY-NNI <tobias.fluehmann@swisscom.com>
2016-12-06 21:21:59 +0100

    fixed merge with basket

--- data/site/views/default/basket.php
+++ data/site/views/default/basket.php
@@ -1,8 +1,8 @@
-<span class="toggle"><i class="fa fa-shopping-cart" aria-hidden="true"></i>
+<span class="toggle" onclick="getBasket()"><i class="fa fa-shopping-cart" aria-hidden="true"></i>
 	<div class="toggle_div hide">
 		<div class="arrow-up"></div>
 		<div class="popup">
-            <div id="basket">
+            <div id="basket" >
 
             </div>
 		</div>
