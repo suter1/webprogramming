50921ca7455ca5546a4b895b57558e94b5907ad5
Raphael Suter <raphael.suter@students.bfh.ch>
2016-12-06 20:39:08 +0100

    some translations and view corrections

--- data/site/views/detail/show.php
+++ data/site/views/detail/show.php
@@ -1,24 +1,27 @@
 <?php require_once("views/default/header.php") ?>
 <?php require_once("views/default/navigation.php") ?>
-<section class="section">
-    <div>
-        <img src="/<?php echo $options['path'] ?>" class="wrap align-left" />
-        <aside class="aside_right">
-            <p>Title: <?php echo $options['title'] ?></p>
-            <p>Price: <?php echo $options['price'] ?></p>
-            <p>
-                <?php
-                if(isset($_SESSION['user_id']) && $options['owner_id'] == $_SESSION['user_id']){
-                    $img_id = $options['id'];
-                    echo "<a href='/upload/$img_id/edit'>Edit</a>";
-                }
-                ?>
-            </p>
-        </aside>
-    </div>
-    <article class="OptSize">
-        <p>Bildgrössenauswahl</p>
-        <article class="MetaData">"Meta data"</article>
-    </article>
-</section>
+<?php require_once 'autoload.php'; ?>
+	<section class="section">
+		<div>
+			<img src="/<?php echo $options['path'] ?>" class="wrap align-left"/>
+			<aside class="aside_right">
+				<p class="title">Title: <?php echo $options['title'] ?></p>
+				<p>CHF <?php echo $options['price'] ?></p>
+				<span class="toggle"><i class="fa fa-shopping-basket" aria-hidden="false"></i></span>
+			</aside>
+		</div>
+		<article class="OptSize">
+			<p>Bildgrössenauswahl</p>
+			<article class="MetaData">"Meta data"</article>
+		</article>
+		<p>
+			<?php
+			if (isset($_SESSION['user_id']) && $options['owner_id'] == $_SESSION['user_id']) {
+				$img_id = $options['id'];
+				$locedit = Localization::find_by(["qualifier" => 'edit', 'lang' => get_language()]);
+				echo "<button onclick='/upload/$img_id/edit'>" . $locedit->getValue() . "</button>";
+			}
+			?>
+		</p>
+	</section>
 <?php require_once("views/default/footer.php") ?>
