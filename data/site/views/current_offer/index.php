a166d5a400dfcc6062cbbdd2aedeb8da4bb8d70f
Flühmann Tobias, INI-ON-NTO-COY-NNI <tobias.fluehmann@swisscom.com>
2017-01-08 21:59:24 +0100

    prevent from null pointer if null

--- data/site/views/current_offer/index.php
+++ data/site/views/current_offer/index.php
@@ -2,13 +2,15 @@
 <?php
 $html = "";
 $html .= "<h4>Today 50% discount for the following pictures!</h4>";
-foreach($options['offer']->pictures() as $picture){
-	$html .= "<div><a href='/detail/". $picture->getId(). "'>
+if(isset($options['offer']) && !is_null($options['offer'])) {
+	foreach ($options['offer']->pictures() as $picture) {
+		$html .= "<div><a href='/detail/" . $picture->getId() . "'>
 	<img src='/" . $picture->getThumbnailPath() . "' />
 	<br>
-	<span>" . $picture->getTitle() . " *** " . money_format('%.2n CHF', $picture->getPrice()*0.5)."</span>
+	<span>" . $picture->getTitle() . " *** " . money_format('%.2n CHF', $picture->getPrice() * 0.5) . "</span>
 	</a>
 	</div>";
+	}
 }
 echo $html;
 ?>
